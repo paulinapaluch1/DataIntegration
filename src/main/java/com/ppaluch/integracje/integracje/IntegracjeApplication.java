97bb3545c4f6c05262a50fbd90f1ec1eb690c9ac
Paulina Paluch <paulina.paluch1@pollub.edu.pl>
2021-03-13 08:39:42 +0100

    init repo

---
+++ src/main/java/com/ppaluch/integracje/integracje/IntegracjeApplication.java
@@ -0,0 +1,13 @@
+package com.ppaluch.integracje.integracje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracjeApplication.class, args);
+	}
+
+}
